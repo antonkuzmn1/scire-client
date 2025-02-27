2623226f6115ba3af8ebdcfdab03dc5f7237ac36
anton <antonkuzmn1@gmail.com>
2025-02-27 23:46:01 +0700

    test pt3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,7 @@ import PageMe from "./pages/PageMe.tsx";
 import Page from "./pages/Page.tsx";
 import PageMessenger from "./pages/PageMessenger.tsx";
 import PageMessengerChat from "./pages/PageMessengerChat.tsx";
-import {WebSocketProvider} from "./WebSocketContext.tsx";
+import { WebSocketProvider } from "./WebSocketContext.tsx";
 
 export interface RoutePageInterface {
     path: string;
@@ -49,16 +49,14 @@ function App() {
     }
 
     return (
-        <>
-            <WebSocketProvider>
-                {!authorized ? <Authorization/> : <RouterProvider router={router}/>}
-            </WebSocketProvider>
+        <WebSocketProvider>
+            {!authorized ? <Authorization/> : <RouterProvider router={router}/>}
 
             <ErrorMessage/>
             <Message/>
 
             <Loading/>
-        </>
+        </WebSocketProvider>
     )
 }
 
